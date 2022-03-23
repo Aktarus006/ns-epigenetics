3fcb4c7065a6a15766cd9ff36fc91f5d19d846fd
Aktarus006 <joachim.bastin@diagenode.com>
2022-03-23 12:41:14 +0100

    config: moving css to assets

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,8 +2,16 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  {{ user }}
 </template>
 
+<script setup lang="ts">
+import { useUserStore } from './store/authStore'
+
+const user = useUserStore()
+
+</script>
+
 <style>
-@import '~/styles/main.css';
+@import '~/assets/styles/main.css';
 </style>
